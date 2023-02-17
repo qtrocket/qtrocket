ef7de152bec1ebfbfa396e6577bf809e61526e7d
Travis Hunter <travis@travishunter.io>
2023-02-16 19:04:06 -0700

    WIP

---
+++ src/model/MotorCase.h
@@ -0,0 +1,23 @@
+#ifndef MODEL_MOTORCASE_H
+#define MODEL_MOTORCASE_H
+
+enum class MotorManufacturerEnum
+{
+   AEROTECH,
+   CESARONI,
+   LOKI,
+   AMW,
+   ESTES
+};
+
+class MotorCase
+{
+public:
+   MotorCase();
+   ~MotorCase();
+
+private:
+
+};
+
+#endif // MODEL_MOTORCASE_H
