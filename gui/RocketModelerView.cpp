5d032a95dce22ae8bcc4389503c8c91e5b42c04a
Travis Hunter <travis@travishunter.io>
2026-06-08 14:54:12 -0500

    Remove qcustomplot widget from bottom of main window. Replace with RocketModelerView (empty for now) widget.

---
+++ gui/RocketModelerView.cpp
@@ -0,0 +1,6 @@
+#include "RocketModelerView.h"
+
+RocketModelerView::RocketModelerView(QWidget* parent) : QWidget(parent)
+{
+
+}
---
+++ gui/RocketModelerView.h
@@ -0,0 +1,25 @@
+#ifndef GUI_ROCKETMODELERVIEW_H
+#define GUI_ROCKETMODELERVIEW_H
+
+/// \cond
+// C headers
+// C++ headers
+// 3rd party headers
+#include <QWidget>
+/// \endcond
+
+// qtrocket headers
+
+/**
+ * @brief RocketModelerView shows the current rocket model
+ *
+ */
+class RocketModelerView : public QWidget
+{
+   Q_OBJECT
+
+public:
+   RocketModelerView(QWidget* parent = nullptr);
+};
+
+#endif // GUI_ROCKETMODELERVIEW_H
